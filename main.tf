47c9d30c129a24fc2c172fe74dd95998d040da54
jiboe <ajibolaoduleye@gmail.com>
2025-01-08 11:40:52 -0500

    modified main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
-resource "aws_instance" "MyInstance_test" {
+resource "aws_instance" "ldap-server" {
     #instance configuration
+    ami = "ami-01816d07b1128cd2d"
+    instance_type = "t2.micro"
+
+    tags = {
+      Name = "ldap-server"
+    }
 }
