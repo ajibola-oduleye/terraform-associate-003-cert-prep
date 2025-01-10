ef87cbf92964bad8a4cff9d5d6dbf4f4214f7067
jiboe <ajibolaoduleye@gmail.com>
2025-01-10 09:13:48 -0500

    updated variables.tf and created terraform.tfvars (in .gitignore)

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@
 variable "bucket_name" {
   description = "Name for the S3 bucket"
   type        = string
-  default     = "terraform-demo-bucket-variables"
+
 }
 
 
@@ -11,9 +11,6 @@ variable "bucket_name" {
 variable "tags" {
   description = "A map of tags for the S3 bucket"
   type        = map(string)
-  default     = {
-    Name        = "terraform-demo-bucket-variables"
-    Environment = "Dev"
-  }
+  
   }
 
